e38570e63a68746cea7f31c7c95f36266fc08ef4
Kundan <kundan kamble>
2022-03-11 07:06:45 -0800

    [kundan] snake ladder added

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+class DiceRoll
+{
+    static void Main(string[] args)
+
+    {
+        Random random = new Random();
+        int dice1=random.Next(1,7);
+        int dice2=random.Next(1,7);
+        Console.WriteLine("Dice 1: " + dice1);
+        Console.WriteLine("Dice 2: " + dice2);
+        
+        if (dice1>dice2)
+        {
+            Console.WriteLine("Dice 1 Wins");
+        }
+        else if(dice2 >dice1)
+        {
+            Console.WriteLine("dice 2 wins");
+
+        }
+        else
+        {
+            Console.WriteLine("draw");
+
+        }
+
+        Console.ReadLine();
+    }
+}
+
+
